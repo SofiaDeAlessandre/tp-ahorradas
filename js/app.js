744035c4f104d03461f360780e4f0969956d6fdb
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2023-12-20 23:15:30 -0300

    add new functions of screen changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 const $ = selector => document.querySelector(selector)
 const $$ = selector => document.querySelectorAll(selector)
 
+const showElement = (selector) => $(selector).classList.remove("hidden")
+const hideElement = (selector) => $(selector).classList.add("hidden")
+
+const initialize = () => {
+    $("#categories-nav").addEventListener("click", () => {
+        showElement("#category-container")
+        hideElement("#main-view")
+        hideElement("#reports-div")
+        hideElement("#form-new-operation")
+    })
+    $("#reports-link").addEventListener("click", () => {
+        showElement("#reports-div")
+        hideElement("#main-view")
+        hideElement("#category-container")
+        hideElement("#form-new-operation")
+    })
+    $("#btn-new-operation").addEventListener("click", () => {
+        showElement("#form-new-operation")
+        hideElement("#main-view")
+    })
+    $("#form-new-operation").addEventListener("click", (e) => {
+        e.preventDefault()
+    })
+}
+window.addEventListener("load", initialize)
 //HAMBURGUER MENU
 
 $("#icon-nav").addEventListener("click", () => {
